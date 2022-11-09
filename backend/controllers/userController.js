97d3057740186ce7ef342decd2fe2566638e9ede
shyarnis <shyarnisgg88@gmail.com>
2022-11-09 23:05:42 +0545

    feat: login user with email and password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -53,7 +53,22 @@ const registerUser = asyncHandler( async (req, res) => {
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler( async (req, res) => {
-    res.json({message: "Login User"})
+    const {email, password} = req.body
+
+    // 1. check by user email
+    const user = await User.findOne({ email })
+
+    // 2. if user and password match send json, else throw error
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid Credentials')
+    }
 })
 
 // @desc    Get user data
