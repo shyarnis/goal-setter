2e69b0af6bd68c3af48788d36fc9ae261ab6c457
shyarnis <shyarnisgg88@gmail.com>
2022-11-10 20:13:16 +0545

    get user data with Bearer Token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,13 @@ const loginUser = asyncHandler( async (req, res) => {
 // @route   GET /api/users/me
 // @access  Private
 const getMe = asyncHandler( async (req, res) => {
-    res.json({message: "Get user display"})
+    const {_id, name, email } = await User.findById(req.user.id)
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    })
 })
 
 // Generate JWT
